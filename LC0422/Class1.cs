519d70141f2ffbc6dab29d42be1678ad5a674165
alexa <alexa@ALEXANDRAS-LAPT>
2024-04-21 22:45:57 -0500

    Add project files.

---
+++ LC0422/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace c
+{
+    public class Student
+    {
+        public string Id;
+        public string Name;
+        public double Grade;
+
+        public string GetInfor()
+        {
+            return $"Id:{Id}, Name:{Name}, Grade:{Grade:F2}";
+        }
+    }
+}
---
+++ LC0422/Program.cs
@@ -0,0 +1,29 @@
+﻿// MIS 3013 001
+// April 22, 2024
+// Alexandra Camarena
+// 113568154
+
+using c;
+
+Console.WriteLine("Dictionary");
+
+//use student id to find the student
+Dictionary<string, Student> stuDict;// complex, 0
+
+stuDict = new Dictionary<string, Student>();
+
+Student stu;
+stu = new Student();
+stu.Id = "S101";
+stu.Name = "Tom";
+stu.Grade = 96.6;
+
+stuDict.Add(stu.Id, stu);
+stu.Id = "S102";
+stu.Name = "Kelly";
+stu.Grade = 90.6;
+
+stuDict.Add(stu.Id, stu);
+
+Console.WriteLine(stuDict["S102"].GetInfor());
+
